8669a0802915820c1d8053b8e975a819d56e1e2e
Wesley Frederickson <6827305+fredwes@users.noreply.github.com>
2023-12-21 16:06:08 -0800

    chore: lint

--- frontend/src/config/app.ts
+++ frontend/src/config/app.ts
@@ -63,7 +63,8 @@ const geyserList: AppGeysersList = {
       stakingToken: StakingToken.UNISWAP_V2,
       rewardToken: RewardToken.SEAM,
       isWrapped: false,
-      poolAddress: 'https://soswap.rai.finance/#/add/0x1C7a460413dD4e964f96D8dFC56E7223cE88CD85/0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
+      poolAddress:
+        'https://soswap.rai.finance/#/add/0x1C7a460413dD4e964f96D8dFC56E7223cE88CD85/0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
       // Staked pool token: https://basescan.org/address/0x11e438b489e9e97e0252acd83aaa0a57ab611911
     },
     {
